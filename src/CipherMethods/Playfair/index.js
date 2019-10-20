dd8660c8e0ba07a745b0da6e764b8861a18932f8
ahmeed2m <2m.fcih@fci.helwan.edu.eg>
2019-10-20 12:53:58 +0200

    Added matrix generation based on key

--- src/CipherMethods/Playfair/index.js
+++ src/CipherMethods/Playfair/index.js
@@ -1,48 +1,55 @@
 import React, {useState,useEffect} from "react";
 import { Input, Button, Typography, Select } from "antd";
 import './style.css';
+import { uniqueString } from "../../Helper";
 const { Paragraph } = Typography;
 const { Option } = Select;
 
 function Playfair(props) {
   const [key,setKey] = useState("");
+  const [matrix, setMatrix ] = useState(props.alphabet)
   const [cipher,setCipher] = useState("");
   const [deCipher,setDeCipher] = useState("");
   const [deCipherText,setDeCipherText] = useState("");
   const [toChange,setToChange] = useState("J");
   const [changeWith,setChangeWith] = useState("I");
+  
+  function handleKeyChange(val){
+    let newKey = uniqueString(val);
+    setKey(newKey);
+  }
+
+  useEffect(()=>{
+    setMatrix(uniqueString(key+props.alphabet))
+  },[key])
+
   return (
-    
+
     <div className="main">
       <h4>Playfair Things</h4>
       <Input
-        onChange={e => setKey(e.target.value)}
+        onChange={e => handleKeyChange(e.target.value)}
         style={{ width: 360 }}
         addonBefore="Your Key"
         value={key}
       />
-      <Button
-        type="dashed"
-        // onClick={()=>{genKey()}}
-        style={{ width: 200 }}
-      >Generate Random Key</Button>
       <div>
         <span>Substitute: </span>
         <Select onChange={(e)=>setToChange(e)} style={{ width: 50 }} dropdownMatchSelectWidth={false} defaultValue={"J"}>
-          {props.alphabet.split('').map((char)=>{
-            return(<Option key={char} value={char}>{char}</Option>)
+          {matrix.split('').map((char)=>{
+            return(<Option key={char} value={char}>{char}</Option>);
           })}
         </Select>
         <span style={{margin:10}}>with</span>
         <Select onChange={(e)=>setChangeWith(e)} style={{ width: 50 }} dropdownMatchSelectWidth={false} defaultValue={"I"}>
           {props.alphabet.split('').map((char)=>{
-            return(<Option key={char} value={char}>{char}</Option>)
+            return(<Option key={char} value={char}>{char}</Option>);
           })}
         </Select>
       </div>
       
       <div className="matrix">
-        {props.alphabet.split('').map((char)=>{
+        {matrix.split('').map((char)=>{
           if(char==changeWith){
             return <span className="double">{changeWith}/{toChange}</span>;
           }if(char==toChange){
